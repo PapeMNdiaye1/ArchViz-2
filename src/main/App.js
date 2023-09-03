f3704e656559c1725bd43658e4ff2d2747175455
PapeMNdiaye1 <pmomar58@gmail.com>
2023-09-03 19:20:08 +0000

    React lazy test -2

--- src/main/App.js
+++ src/main/App.js
@@ -4,31 +4,30 @@ import React, { useState, Suspense } from 'react';
 import { Routes, BrowserRouter, Route, } from "react-router-dom";
 // import OtherComponent from './OtherComponent';
 
-// import TopBare from './TopBare';
 import { Accueil } from './Pages/Accueil';
 import { TheFooter } from './Pages/Accueil';
-// import Gallery from './Pages/Galerie';
-import Travaux from './Pages/Travaux';
 import Service from './Pages/Service';
-import Contact from './Pages/Contact';
-
-// import Interior1 from './Pages/Projects/Interior_Design';
-import SmallHouse1 from './Pages/Projects/Small_House_1';
-import Hangar1 from './Pages/Projects/Hangar_1';
-import Touba from './Pages/Projects/Touba';
-import Pharmacie1 from './Pages/Projects/Pharmacie_1';
-import Pharmacie2 from './Pages/Projects/Pharmacie_2';
-import Gym from './Pages/Projects/Gym';
-import City1 from './Pages/Projects/Cité';
-import MSAD from './Pages/Projects/MSAD';
-import Villa from './Pages/Projects/Villa_A';
-import Villa_Saly from './Pages/Projects/Villa_B';
-import VillaTerangaAlAmin from './Pages/Projects/Villa_Teranga_Al_Amin';
 
 const TopBare = React.lazy(() => import('./TopBare'));
+
+const Travaux = React.lazy(() => import('./Pages/Travaux'));
+const Contact = React.lazy(() => import('./Pages/Contact'));
+
+
+
 const Gallery = React.lazy(() => import('./Pages/Galerie'));
 const Interior1 = React.lazy(() => import('./Pages/Projects/Interior_Design'));
-// const Interior1 = React.lazy(() => import('./Pages/Projects/Interior_Design'));
+const SmallHouse1 = React.lazy(() => import('./Pages/Projects/Small_House_1'));
+const Hangar1 = React.lazy(() => import('./Pages/Projects/Hangar_1'));
+const Touba = React.lazy(() => import('./Pages/Projects/Touba'));
+const Pharmacie1 = React.lazy(() => import('./Pages/Projects/Pharmacie_1'));
+const Pharmacie2 = React.lazy(() => import('./Pages/Projects/Pharmacie_2'));
+const Gym = React.lazy(() => import('./Pages/Projects/Gym'));
+const City1 = React.lazy(() => import('./Pages/Projects/Cité'));
+const MSAD = React.lazy(() => import('./Pages/Projects/MSAD'));
+const Villa = React.lazy(() => import('./Pages/Projects/Villa_A'));
+const Villa_Saly = React.lazy(() => import('./Pages/Projects/Villa_B'));
+const VillaTerangaAlAmin = React.lazy(() => import('./Pages/Projects/Villa_Teranga_Al_Amin'));
 
 function App() {
 
@@ -63,9 +62,9 @@ function App() {
             <Route exact path="/"
               element={<Accueil GetImageToApp={GetImage} />}
             />
-            <Route exact path="/Travaux" element={<Travaux />} />
+            <Route exact path="/Travaux" element={<Suspense fallback={<div></div>}><Travaux /></Suspense>} />
             <Route exact path="/Services" element={<Service />} />
-            <Route exact path="/Contact" element={<Contact />} />
+            <Route exact path="/Contact" element={<Suspense fallback={<div></div>}><Contact /></Suspense>} />
 
             <Route exact path="/Galerie" element={
               <Suspense fallback={<div>Chargement...</div>}>
@@ -80,18 +79,18 @@ function App() {
 
             {/* !############################################## */}
 
-            <Route exact path="/Interior1" element={<Suspense fallback={<div>Chargement...</div>}><Interior1 /></Suspense>} />
-            <Route exact path="/SmallHouse1" element={<SmallHouse1 />} />
-            <Route exact path="/Hangar1" element={<Hangar1 />} />
-            <Route exact path="/Touba" element={<Touba />} />
-            <Route exact path="/Pharmacie1" element={<Pharmacie1 />} />
-            <Route exact path="/Pharmacie2" element={<Pharmacie2 />} />
-            <Route exact path="/Gym" element={<Gym />} />
-            <Route exact path="/City1" element={<City1 />} />
-            <Route exact path="/MSAD" element={<MSAD />} />
-            <Route exact path="/Villa" element={<Villa />} />
-            <Route exact path="/Villa-Saly" element={<Villa_Saly />} />
-            <Route exact path="/VillaTerangaAlAmin" element={<VillaTerangaAlAmin />} />
+            <Route exact path="/Interior1" element={<Suspense fallback={<div></div>}><Interior1 /></Suspense>} />
+            <Route exact path="/SmallHouse1" element={<Suspense fallback={<div></div>}><SmallHouse1 /></Suspense>} />
+            <Route exact path="/Hangar1" element={<Suspense fallback={<div></div>}><Hangar1 /></Suspense>} />
+            <Route exact path="/Touba" element={<Suspense fallback={<div></div>}><Touba /></Suspense>} />
+            <Route exact path="/Pharmacie1" element={<Suspense fallback={<div></div>}><Pharmacie1 /></Suspense>} />
+            <Route exact path="/Pharmacie2" element={<Suspense fallback={<div></div>}><Pharmacie2 /></Suspense>} />
+            <Route exact path="/Gym" element={<Suspense fallback={<div></div>}><Gym /></Suspense>} />
+            <Route exact path="/City1" element={<Suspense fallback={<div></div>}><City1 /></Suspense>} />
+            <Route exact path="/MSAD" element={<Suspense fallback={<div></div>}><MSAD /></Suspense>} />
+            <Route exact path="/Villa" element={<Suspense fallback={<div></div>}><Villa /></Suspense>} />
+            <Route exact path="/Villa-Saly" element={<Suspense fallback={<div></div>}><Villa_Saly /></Suspense>} />
+            <Route exact path="/VillaTerangaAlAmin" element={<Suspense fallback={<div></div>}><VillaTerangaAlAmin /></Suspense>} />
 
           </Routes>
           <TheFooter />
