0490611a0233057d679b77f92e948bda6e812031
PapeMNdiaye1 <pmomar58@gmail.com>
2023-09-28 14:45:00 +0000

    UpDating Appartement-F4-A-3

--- src/main/App.js
+++ src/main/App.js
@@ -10,6 +10,18 @@ const TopBare = React.lazy(() => import('./TopBare'));
 const Travaux = React.lazy(() => import('./Pages/Travaux'));
 const Contact = React.lazy(() => import('./Pages/Contact'));
 
+//!###############################################################
+function TheLoader() {
+  return (
+    <div className='hover_loader_container'>
+      <div className='hover_loader'>
+        <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+      </div>
+    </div>
+  );
+}
+
+//!###############################################################
 
 
 const Gallery = React.lazy(() => import('./Pages/Galerie'));
@@ -25,6 +37,10 @@ const MSAD = React.lazy(() => import('./Pages/Projects/MSAD'));
 const Villa = React.lazy(() => import('./Pages/Projects/Villa_A'));
 const Villa_Saly = React.lazy(() => import('./Pages/Projects/Villa_B'));
 const VillaTerangaAlAmin = React.lazy(() => import('./Pages/Projects/Villa_Teranga_Al_Amin'));
+
+
+
+//!###############################################################
 
 function App() {
 
@@ -52,18 +68,14 @@ function App() {
       <div className="App">
         <TopBare onChangeTab={changeTab} />
         <div className='App_container'>
-          <div className='hover_loader_container'>
-            <div className='hover_loader'>
-              <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
-            </div>
-          </div>
+          <TheLoader />
           <Routes>
             <Route exact path="/"
               element={<Accueil GetImageToApp={GetImage} />}
             />
-            <Route exact path="/Travaux" element={<Suspense fallback={<div className='the-suspense' ></div>}><Travaux /></Suspense>} />
+            <Route exact path="/Travaux" element={<Suspense fallback={<TheLoader />}><Travaux /></Suspense>} />
             <Route exact path="/Services" element={<Service />} />
-            <Route exact path="/Contact" element={<Suspense fallback={<div className='the-suspense' ></div>}><Contact /></Suspense>} />
+            <Route exact path="/Contact" element={<Suspense fallback={<TheLoader />}><Contact /></Suspense>} />
 
             <Route exact path="/Galerie" element={
               <Suspense fallback={<div className='the-suspense' >Chargement...</div>}>
@@ -78,18 +90,18 @@ function App() {
 
             {/* !############################################## */}
 
-            <Route exact path="/Interior1" element={<Suspense fallback={<div className='the-suspense' ></div>}><Interior1 /></Suspense>} />
-            <Route exact path="/SmallHouse1" element={<Suspense fallback={<div className='the-suspense' ></div>}><SmallHouse1 /></Suspense>} />
-            <Route exact path="/Hangar1" element={<Suspense fallback={<div className='the-suspense' ></div>}><Hangar1 /></Suspense>} />
-            <Route exact path="/Touba" element={<Suspense fallback={<div className='the-suspense' ></div>}><Touba /></Suspense>} />
-            <Route exact path="/Pharmacie1" element={<Suspense fallback={<div className='the-suspense' ></div>}><Pharmacie1 /></Suspense>} />
-            <Route exact path="/Appartement-F4-A" element={<Suspense fallback={<div className='the-suspense' ></div>}><AppartementFA /></Suspense>} />
-            <Route exact path="/Gym" element={<Suspense fallback={<div className='the-suspense' ></div>}><Gym /></Suspense>} />
-            <Route exact path="/City1" element={<Suspense fallback={<div className='the-suspense' ></div>}><City1 /></Suspense>} />
-            <Route exact path="/MSAD" element={<Suspense fallback={<div className='the-suspense' ></div>}><MSAD /></Suspense>} />
-            <Route exact path="/Villa" element={<Suspense fallback={<div className='the-suspense' ></div>}><Villa /></Suspense>} />
-            <Route exact path="/Villa-Saly" element={<Suspense fallback={<div className='the-suspense' ></div>}><Villa_Saly /></Suspense>} />
-            <Route exact path="/VillaTerangaAlAmin" element={<Suspense fallback={<div className='the-suspense' ></div>}><VillaTerangaAlAmin /></Suspense>} />
+            <Route exact path="/Interior1" element={<Suspense fallback={<TheLoader />}><Interior1 /></Suspense>} />
+            <Route exact path="/SmallHouse1" element={<Suspense fallback={<TheLoader />}><SmallHouse1 /></Suspense>} />
+            <Route exact path="/Hangar1" element={<Suspense fallback={<TheLoader />}><Hangar1 /></Suspense>} />
+            <Route exact path="/Touba" element={<Suspense fallback={<TheLoader />}><Touba /></Suspense>} />
+            <Route exact path="/Pharmacie1" element={<Suspense fallback={<TheLoader />}><Pharmacie1 /></Suspense>} />
+            <Route exact path="/Appartement-F4-A" element={<Suspense fallback={<TheLoader />}><AppartementFA /></Suspense>} />
+            <Route exact path="/Gym" element={<Suspense fallback={<TheLoader />}><Gym /></Suspense>} />
+            <Route exact path="/City1" element={<Suspense fallback={<TheLoader />}><City1 /></Suspense>} />
+            <Route exact path="/MSAD" element={<Suspense fallback={<TheLoader />}><MSAD /></Suspense>} />
+            <Route exact path="/Villa" element={<Suspense fallback={<TheLoader />}><Villa /></Suspense>} />
+            <Route exact path="/Villa-Saly" element={<Suspense fallback={<TheLoader />}><Villa_Saly /></Suspense>} />
+            <Route exact path="/VillaTerangaAlAmin" element={<Suspense fallback={<TheLoader />}><VillaTerangaAlAmin /></Suspense>} />
 
           </Routes>
           <TheFooter />
@@ -98,5 +110,9 @@ function App() {
     </BrowserRouter>
   );
 }
+
+
+
+
 
 export default App;
