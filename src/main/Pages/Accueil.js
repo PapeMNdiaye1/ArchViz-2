647c0098deeac14222d8ee3fcfccc4e1bed19547
PapeMNdiaye1 <pmomar58@gmail.com>
2023-10-20 22:47:46 +0000

    modiff-1

--- src/main/Pages/Accueil.js
+++ src/main/Pages/Accueil.js
@@ -124,25 +124,25 @@ function Accueil({ GetImageToApp }) {
                     description={"Des designs Modernes, épurés, minimalistes etc.… nous mettons en œuvre différentes approches pour une optimisation de vos espaces extérieurs et intérieurs. Pour se faire nous utilisons différents logiciels avec les dernières innovations pour des rendus d’images à la fois réalistes et immersives."}
                     color={'#4c525c'} textcolor={'#f1f1f1'} theKey={4} link={'/Villa'} side={'left'} The_Experience={true} The_Experience_Link={'https://archviz-villa-astan.netlify.app/'} />
 
-                <OneBigProject title={'Décoration Intérieur'} image={ImageProjectC2}
+                {/* <OneBigProject title={'Décoration Intérieur'} image={ImageProjectC2}
                     description={"À l'aide des logiciels de dernière génération et de notre savoir-faire dans le domaine de l’architecture d’intérieur. Nous vous aidons dans l’aménagement de vos espaces."}
-                    color={'#EAEAEA'} textcolor={'#222'} theKey={5} side={'right'} link={'/Interior1'} The_Experience={true} The_Experience_Link={'https://papemndiaye1.github.io/ArchViz_Interior_Design_1/'} />
+                    color={'#EAEAEA'} textcolor={'#222'} theKey={5} side={'right'} link={'/Interior1'} The_Experience={true} The_Experience_Link={'https://papemndiaye1.github.io/ArchViz_Interior_Design_1/'} /> */}
 
                 <OneBigProject title={'Design pour les Commerces'} image={ImageProjectE3}
                     description={'Nous avons une grande expérience dans l’agencement de point de vente, et l’aménagement de bureaux. Quelle que soit la typologie du point de vente, sa vocation, sa stratégie et son positionnement.'}
-                    color={'#F4EFE4'} textcolor={'#222'} theKey={6} side={'left'} The_Experience={false} link={'/Pharmacie1'} />
+                    color={'#F4EFE4'} textcolor={'#222'} theKey={5} side={'right'} The_Experience={false} link={'/Pharmacie1'} />
 
                 <OneBigProject title={'Visualisation de projets immobiliers'} image={ImageProjectI2}
                     description={"Des designs Modernes, épurés, etc… nous mettons en œuvre différentes approches pour une optimisation de vos espaces extérieurs et intérieurs. Pour se faire nous utilisons différents logiciels avec les dernières innovations pour des rendus d’images à la fois réalistes et immersives."}
-                    color={'#EAEAEA'} textcolor={'#222'} The_Experience={true} The_Experience_Link={'https://villa-r-1.netlify.app/'} theKey={7} link={'/Villa-Saly'} side={'right'} />
+                    color={'#EAEAEA'} textcolor={'#222'} The_Experience={true} The_Experience_Link={'https://villa-r-1.netlify.app/'} theKey={6} side={'left'} link={'/Villa-Saly'} />
 
                 <OneBigProject title={'Espace Sportif'} image={ImageProjectK1}
                     description={"Nous avons une vaste expérience dans l'aménagement d'espaces communs et de bureaux, quelle que soient leur typologie, leur vocation, leur stratégie et leur positionnement."}
-                    color={'#c6b282'} textcolor={'#222'} theKey={8} link={'/Gym'} side={'left'} The_Experience={false} />
+                    color={'#c6b282'} textcolor={'#222'} theKey={7} side={'right'} link={'/Gym'} The_Experience={false} />
 
                 <OneBigProject title={'La 3D dans le domaine des Industries'} image={ImageProjectF2}
                     description={"Le rendu et l'animation avec des temps de production de plus en plus rapide ont ouvert la porte à un monde de variations infinies dans la conception graphique adaptée à vos demandes uniques."}
-                    color={'#F1F1F1'} textcolor={'#222'} theKey={9} The_Experience={false} link={'/Hangar1'} side={'right'} />
+                    color={'#F1F1F1'} textcolor={'#222'} The_Experience={false} link={'/Hangar1'} theKey={8} side={'left'} />
             </section>
             <section className='galerie_presantation'>
                 <Link className='nos_réalisations' to={'./Travaux'}>
