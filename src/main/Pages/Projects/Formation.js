3fb9c3d7e5fec5a9c81f5079d39b2e77ebfd2231
PapeMNdiaye1 <pmomar58@gmail.com>
2024-10-13 15:29:44 +0000

    testing commit4

--- src/main/Pages/Projects/Formation.js
+++ src/main/Pages/Projects/Formation.js
@@ -1,11 +1,11 @@
 import { React, useEffect, useState, Fragment } from 'react';
 
 import ImageFormation1 from '../../Style/Images/SVG/Formation-1.png';
+import ImageFormation2 from '../../Style/Images/SVG/Formation-2.png';
 
 function SmallHouse1({ }) {
     // const [TheImageContainer, setTheImageContainer] = useState(false);
     // const [TheImageInTheCoFontainer, setTheImageInTheContainer] = useState(ImageProjectC1);
-
     useEffect(() => {
         let ToDisplayonBLoade = document.querySelector(".before-loader")
         ToDisplayonBLoade.style.display = 'none';
@@ -16,7 +16,6 @@ function SmallHouse1({ }) {
         let AccueilContainer = document.querySelector(".App_container")
         AccueilContainer.scrollTop = 0;
 
-
         let hoverLoaderContainer = document.querySelector(".hover_loader_container")
         hoverLoaderContainer.style.display = 'flex';
 
@@ -24,13 +23,10 @@ function SmallHouse1({ }) {
             hoverLoaderContainer.style.display = '';
         }, 1500);
 
-
         return () => {
-
             Page_slider.style.opacity = `1`;
             ToDisplayonBLoade.style.display = 'flex';
             AccueilContainer.scrollTop = 0;
-
         }
     }, []);
 
@@ -42,27 +38,42 @@ function SmallHouse1({ }) {
     //         setTheImageContainer(true);
     //     }
     // }
-    
+
     return (
         <Fragment>
-       
+
             <div className='Formation'>
 
-                 <div className='formation_section_1'>
-                    <img  src={ImageFormation1} width='100%' />
-                        <h1 className='project_title'>
-                       Formation en 3D
+                <div className='formation_section_1'>
+                    <img src={ImageFormation1} width='100%' />
+                    <h1 className='project_title'>
+                        Formation en 3D
                     </h1>
-                  
-                </div>
-                 <div className='formation_section_2'>
-                    hgghh
-                </div>
-                 <div className='formation_section_3'>
-                    hgghh
-                </div>
+                    <h1 className='project_title-2'>
+                        Architecture visualisation
+                    </h1>
 
-            
+                </div>
+                <div className='formation_section_2'>
+                    <img src={ImageFormation2} width='100%' />
+                    <h1 className='project_title'>
+                        Développer <br/>
+                        Vos Compétences <br/>
+                        En Visualisation <br/>
+                         Architecturale 3D
+                    </h1>
+                    <p>
+                        Une formation complete de 4 mois de comportent <br />
+                        des cour live sur Discord un canale question<br />
+                        ouvert h24 et de resouse gratuit (SetUp logiciels,<br />
+                        Models 3D , HDRI , PBR . . .)
+                    </p>
+                </div>
+                <div className='formation_section_3'>
+                    hgghh
+                          </div>
+
+
             </div>
         </Fragment>
     );
